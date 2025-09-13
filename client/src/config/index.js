142a881945601b775db6e59d7ea76837e4307d60
Sahillrathore <sahil921031@gmail.com>
2025-09-13 19:32:19 +0530

    placeholder chnge

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -122,21 +122,21 @@ export const shoppingViewHeaderMenuItems = [
     label: "Women",
     path: "/shop/listing",
   },
-  {
-    id: "kids",
-    label: "Kids",
-    path: "/shop/listing",
-  },
+  // {
+  //   id: "kids",
+  //   label: "Kids",
+  //   path: "/shop/listing",
+  // },
   {
     id: "footwear",
     label: "Footwear",
     path: "/shop/listing",
   },
-  {
-    id: "accessories",
-    label: "Accessories",
-    path: "/shop/listing",
-  },
+  // {
+  //   id: "accessories",
+  //   label: "Accessories",
+  //   path: "/shop/listing",
+  // },
   {
     id: "search",
     label: "Search",
